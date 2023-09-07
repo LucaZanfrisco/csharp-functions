e215d77cc0c6d1f1db2dac39473a7d3b24050f82
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-07 15:27:46 +0200

    Funzioni: stampa di un array e elevazione al quadrato di un numero

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// FUNZIONI
+
+/* 
+ * void StampaArray(int[] array): che preso un array di numeri interi, 
+ * stampa a video il contenuto dell’array in questa forma “[elemento 1, elemento 2, elemento 3, ...]”. 
+*/
+void StampaArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write((i < array.Length - 1) ? $"{array[i]}, " : $"{array[i]}");
+    }
+
+    Console.WriteLine("]");
+}
+
+/*
+ * int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro.
+*/
+
+int Quadrato(int numero)
+{
+    return numero *= numero;
+}
